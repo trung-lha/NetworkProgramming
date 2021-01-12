d75a984025b671dd71e06ed9aa1c8392d0257fa7
Le Hoang Anh Trung <trungcriss99@gmail.com>
2021-01-12 15:54:05 +0700

    Execution time exam

--- Project_ver2/test.c
+++ Project_ver2/test.c
@@ -1,12 +1,21 @@
-#include <string.h>
-#include <stdio.h>
+#include <stdio.h> 
+#include <time.h> 
 #include <stdlib.h>
+int main () { 
+   time_t start_t, end_t; 
+   double diff_t; 
+   char *ans;
+   printf("Bắt đầu chương trình...\n"); 
+   time(&start_t);
+   for(int i=0;i<5;i++){
+      ans = (char*)malloc(sizeof(char));
+      scanf("%s",ans);
+   }
 
-int main()
-{
-   char *ans=(char*)malloc(sizeof(char));
-//    getchar();
-   scanf("%s",ans);
-   printf("ban vua nhap: %s\n",ans);
-   return(0);
+   time(&end_t);
+   diff_t = (difftime(end_t, start_t)); 
+   int phut = diff_t/60;
+   printf("Thời gian thực thi là %d phút %.0f giây\n",phut, (diff_t - phut*60)); 
+   printf("Thoát chương trình...\n"); 
+   return(0); 
 }
